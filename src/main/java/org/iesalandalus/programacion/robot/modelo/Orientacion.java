bf5174d681286b4e9e8bee45f20013c8990f2e6c
Alberto <115214725+ualamc158@users.noreply.github.com>
2024-11-22 13:51:17 +0100

    Enumeration Orientación terminado con tests pasados satisfactoriamente

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -1,5 +1,22 @@
 package org.iesalandalus.programacion.robot.modelo;
 
 public enum Orientacion {
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste");
 
+    private String nombre;
+    private Orientacion(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(nombre);
+    }
 }
