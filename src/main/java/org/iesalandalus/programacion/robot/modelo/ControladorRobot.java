a43c5696950201103e316cd3a9596f07c13bbd5e
Alberto <115214725+ualamc158@users.noreply.github.com>
2024-11-27 10:44:16 +0100

    Clase ControladorRobot terminada con tests pasados exitosamente

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -0,0 +1,22 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public class ControladorRobot {
+    private Robot robot;
+
+    public ControladorRobot(Robot robot) {
+        this.robot = new Robot(robot);
+    }
+
+    public Robot getRobot() {
+        return new Robot(robot);
+    }
+
+    public void ejecutar(char comando) {
+        switch (comando) {
+            case 'A', 'a' -> robot.avanzar();
+            case 'D', 'd' -> robot.girarALaDerecha();
+            case 'I', 'i' -> robot.girarALaIzquierda();
+            default -> throw new RobotExcepcion("Comando desconocido.");
+        }
+    }
+}
