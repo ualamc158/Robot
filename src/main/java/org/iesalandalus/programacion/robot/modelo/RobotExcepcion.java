2ade38d786771d44a72c0f9c10fb15c25d1c907a
Alberto <115214725+ualamc158@users.noreply.github.com>
2024-11-27 10:43:50 +0100

    Excepción RobotExcepcion terminada con tests pasados exitosamente

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/RobotExcepcion.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public class RobotExcepcion extends RuntimeException {
+    public RobotExcepcion(String message) {
+        super(message);
+    }
+}
